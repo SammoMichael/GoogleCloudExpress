01ccc7288a86ff9678293f785ac7716916b96d5a
Sam Michael <sammomichael@gmail.com>
2019-03-22 21:27:52 -0400

    deploy GAE

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('/express_backend', (req, res) => {
+  res.send({
+    express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'
+  });
+});
+app.get("/", (req, res) => {
+  res.send({ status: 'Hello World' });
+});
+
+
+const server = app.listen(process.env.PORT || 8080, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`Example app listening at http://${host}:${port}`);
+});
